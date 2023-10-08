6d047fa1adc57680d8ee2c30307efb75a25d3bd5
Hyeonjun Park <phjppo0918@gmail.com>
2023-10-08 16:14:43 +0900

    feat : 배경색 및 헤더 변경

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -7,17 +7,13 @@
         Title,
         AutoAdjust,
     } from '@smui/top-app-bar';
-    import IconButton from '@smui/icon-button';
 
     let topAppBar;
 </script>
 
 
-<Button color="secondary" variant="raised">
-    <Label>signin</Label>
-</Button>
 
-<TopAppBar bind:this={topAppBar} variant="short">
+<TopAppBar color="secondary" bind:this={topAppBar} variant="short">
     <Row>
         <Section>
             <Title>QBOX</Title>
@@ -26,24 +22,9 @@
             <Button variant="raised">
                 <Label>signup</Label>
             </Button>
-            <Button color="secondary" variant="raised">
+            <Button color="primary" variant="outlined">
                 <Label>signin</Label>
             </Button>
         </Section>
     </Row>
 </TopAppBar>
-<AutoAdjust {topAppBar}>
-
-</AutoAdjust>
-
-<style>
-    /* Hide everything above this component. */
-    :global(#smui-app),
-    :global(body),
-    :global(html) {
-        display: block !important;
-        height: auto !important;
-        width: auto !important;
-        position: static !important;
-    }
-</style>
