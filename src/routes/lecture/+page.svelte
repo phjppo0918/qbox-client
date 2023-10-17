c69a87c7841926c93e71885f96b2eb837f103115
Hyeonjun Park <phjppo0918@gmail.com>
2023-10-17 21:17:19 +0900

    feat : 강의 부분 목업 제작

---
+++ src/routes/lecture/+page.svelte
@@ -0,0 +1,35 @@
+<script >
+    import Card from "@smui/card";
+</script>
+
+<h1>강의 목록</h1>
+
+<div class="card-display">
+    <div class="card-container">
+        <Card color="primary" variant="raised" padded>웹프로그래밍기초</Card>
+    </div>
+    <div class="card-container">
+        <Card color="primary" variant="raised" padded>웹프로그래밍응용</Card>
+    </div>
+    <div class="card-container">
+        <Card color="primary" variant="raised" padded>프로그래밍과데이터분석</Card>
+    </div>
+</div>
+
+
+<style>
+    .card-display {
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+        height: 70vh;
+        flex-direction: row;
+        flex-wrap: wrap;
+        align-content: space-around;
+    }
+
+    .card-container {
+        height: 5vh;
+        width: 80vw;
+    }
+</style>
